ed477a2508bdad60a585568c87cb84561648b626
Mabelle Zhang <mabelle.zhang@duke.edu>
2020-07-21 18:42:59 -0500

    pulling KLE into R

--- ay/app.R
+++ ay/app.R
@@ -3,20 +3,20 @@
 
 library(shiny)
 library(dplyr)
-library(rsconnect)
 library(ggplot2)
 library(tidyr)
-library(stringr)
 library(readxl)
 library(formattable)
+library(plotly)
 
 ?geom_bar
 
-#myaydata <- read.csv("ay/Data/MyAYData.csv", stringsAsFactors = FALSE)
-#myaydata.long <- myaydata %>% gather(Age_Group, Count, Women_ReproductiveAge, Young_Adolescents, Youth, Older_Adolescents, Older_Youth)
+aypopdata <- read_excel("ay/Data/CleanedAYData.xlsx", sheet = "AYPOP")
+aypopdata.long <- aypopdata %>% gather(Age_Group,Count,`Young Adolescents (10-14)`,`Older Adolescents (15-19)`,`Older Youth (20-24)`)
 
-aypopdata <- read_excel("Data/CleanedAYData.xlsx", sheet = "AYPOP")
-aypopdata.long <- aypopdata %>% gather(Age_Group,Count,`Women of Reproductive Age (15-49)`,`Young Adolescents (10-14)`,`Older Adolescents (15-19)`,`Older Youth (20-24)`,`Youth (15-24)`)
+kle_age <- read_excel("ay/Data/CleanedAYData.xlsx", sheet = "KLEAgeEvents")
+
+kle_marriage <- read_excel("ay/Data/CleanedAYData.xlsx", sheet = "KLEMarriage")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -32,43 +32,46 @@ ui <- fluidPage(
                         choices = aypopdata.long$Country)
         ),
 
-        # Show a plot of the generated distribution
+        # Shows the plots created in server function
         mainPanel(
-           plotOutput("graph")
-           #plotOutput("wide")
+           plotOutput("graph"),
+           plotOutput("wide")
         )
     )
 )
 
-# Draw Bargraph
+# Draw Bargraphs and Figures
 server <- function(input, output) {
    
-    # res_data <- reactive({
-    #     res <- myaydata.long %>% filter(myaydata.long$Country == input$country)
-    # })
-    # 
+   #createa reactive data set to build bar graph with
     res_data <- reactive({
         res <- aypopdata.long %>% filter(aypopdata.long$Country == input$country)
     })
+    
+    small_res <- reactive({
+        res1 <- aypopdata %>% filter(aypopdata$Country == input$country)
+    })
+    
+    #designs the bar graphs
     output$graph <- renderPlot({
-
-        # ggplot(res_data(), aes(x=input$country, y = Count, fill = Age_Group)) + geom_bar(position="stack", stat="identity") +
-        #    labs(x="Country", y="Count")
         
-        onebar <- (ggplot(res_data(), aes(Country, Count, fill = Age_Group)) + geom_bar(stat = "identity")
+        bar_one <- (ggplot(res_data(), aes(Country, Count, fill = Age_Group)) + geom_bar(stat = "identity")
         + labs(x="Country", y="Ages 15 to 49"))
         
-        onebar + theme_classic()
-
-        # onebar <- (ggplot(res_data(), aes(Age_Group, Count)) + geom_bar(stat = "identity")
-        #            + labs(x= "Country", y="Count"))
-
+        bar_one + theme_classic() + coord_flip()
     })
- 
-    # output$wide <- renderPlot({
-    #     ggplot(res_data(), aes(x=Age_Group, y= Count)) + geom_bar(position="stack", stat="identity") +
-    #         labs(x="Country", y="Ages 15 to 49")
-    #})
+   
+     #designs the bar graphs
+    output$wide <- renderPlot({
+    
+        bartwo <- (ggplot(small_res(), aes(Country, `Women of Reproductive Age (15-49)`)) + geom_bar(stat = "identity")
+                             + labs(x= "Country", y="Count"))
+        bartwo + theme_classic() + coord_flip()
+    })
+    
+    
+    #timeline
+    
 }
 
 # Run the application 
