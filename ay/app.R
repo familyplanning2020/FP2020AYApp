cd2e8bee7fe9bbdd65492f9ba3dbb27c35de0db3
Mabelle Zhang <mabelle.zhang@duke.edu>
2020-07-21 16:41:13 -0500

    stacked bar graph with xlsx file

--- ay/app.R
+++ ay/app.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#hello, this is mabelle. so sorry. very bad at commenting code. if you download this and are confused, i am very sorry
+#will comment when i have the chance
 
 library(shiny)
 library(dplyr)
@@ -13,9 +7,16 @@ library(rsconnect)
 library(ggplot2)
 library(tidyr)
 library(stringr)
+library(readxl)
+library(formattable)
 
-myaydata <- read.csv("Data/MyAYData.csv", stringsAsFactors = FALSE)
-myaydata.long <- myaydata %>% gather(Age_Group, Count, Women_ReproductiveAge, Young_Adolescents, Youth, Older_Adolescents, Older_Youth)
+?geom_bar
+
+#myaydata <- read.csv("ay/Data/MyAYData.csv", stringsAsFactors = FALSE)
+#myaydata.long <- myaydata %>% gather(Age_Group, Count, Women_ReproductiveAge, Young_Adolescents, Youth, Older_Adolescents, Older_Youth)
+
+aypopdata <- read_excel("Data/CleanedAYData.xlsx", sheet = "AYPOP")
+aypopdata.long <- aypopdata %>% gather(Age_Group,Count,`Women of Reproductive Age (15-49)`,`Young Adolescents (10-14)`,`Older Adolescents (15-19)`,`Older Youth (20-24)`,`Youth (15-24)`)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -28,13 +29,13 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput("country",
                         "Select Country",
-                        choices = myaydata$Country)
+                        choices = aypopdata.long$Country)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("graph"),
-           plotOutput("wide")
+           plotOutput("graph")
+           #plotOutput("wide")
         )
     )
 )
@@ -42,19 +43,32 @@ ui <- fluidPage(
 # Draw Bargraph
 server <- function(input, output) {
    
+    # res_data <- reactive({
+    #     res <- myaydata.long %>% filter(myaydata.long$Country == input$country)
+    # })
+    # 
     res_data <- reactive({
-        res <- myaydata.long %>% filter(myaydata.long$Country == input$country)
+        res <- aypopdata.long %>% filter(aypopdata.long$Country == input$country)
     })
     output$graph <- renderPlot({
 
-        ggplot(res_data(), aes(x=input$country, y= Count, fill = Age_Group)) + geom_bar(position="stack", stat="identity") +
-           labs(x="Country", y="Count") + ylim(0, 360000000)
+        # ggplot(res_data(), aes(x=input$country, y = Count, fill = Age_Group)) + geom_bar(position="stack", stat="identity") +
+        #    labs(x="Country", y="Count")
+        
+        onebar <- (ggplot(res_data(), aes(Country, Count, fill = Age_Group)) + geom_bar(stat = "identity")
+        + labs(x="Country", y="Ages 15 to 49"))
+        
+        onebar + theme_classic()
+
+        # onebar <- (ggplot(res_data(), aes(Age_Group, Count)) + geom_bar(stat = "identity")
+        #            + labs(x= "Country", y="Count"))
+
     })
-    
-    output$wide <- renderPlot({
-        ggplot(res_data(), aes(x=Age_Group, y= Count)) + geom_bar(position="stack", stat="identity") +
-            labs(x="Country", y="Ages 15 to 49")
-    })
+ 
+    # output$wide <- renderPlot({
+    #     ggplot(res_data(), aes(x=Age_Group, y= Count)) + geom_bar(position="stack", stat="identity") +
+    #         labs(x="Country", y="Ages 15 to 49")
+    #})
 }
 
 # Run the application 
